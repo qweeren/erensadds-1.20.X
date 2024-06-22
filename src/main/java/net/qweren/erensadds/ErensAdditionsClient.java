5691f525207a94181baf97cde8b72732ee6b35d4
Eren <erenyordem2006@gmail.com>
2024-06-22 16:11:26 +0300

    Initial commit

---
+++ src/main/java/net/qweren/erensadds/ErensAdditionsClient.java
@@ -0,0 +1,10 @@
+package net.qweren.erensadds;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ErensAdditionsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/qweren/erensadds/ErensAdditionsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.qweren.erensadds;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ErensAdditionsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
